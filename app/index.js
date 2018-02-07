9f9c482b3e5187e051ba28c6f0c81c20692920f8
Carduelis <pavepy@gmail.com>
2018-02-07 18:41:05 +0300

    left menu, top bar, screen-splashes

--- app/index.js
+++ app/index.js
@@ -1,9 +1,22 @@
 import "./styles/style.less";
-import $ from 'jquery';
-import 'fullpage.js/dist/jquery.fullPage.js';
+import $ from "jquery";
+import "fullpage.js/dist/jquery.fullPage.js";
 const rootElement = document.getElementById("root");
-console.log('kek');
 
 $(document).ready(function() {
-	$('#fullpage').fullpage();
-})
+	$("#fullpage").fullpage();
+	$(".screen-card").each(function() {
+		const $card = $(this);
+		const SCREEN_WIDTH = 1024;
+		const SCREEN_HEIGHT = 768;
+		const css = {
+			width: $card.data("width") / SCREEN_WIDTH * $card.width(),
+			height: $card.data("height") / SCREEN_HEIGHT * $card.height()
+		};
+		$card.css({
+			"justify-content": $card.data("x"),
+			"align-items": $card.data("y")
+		});
+		$card.find("span").css(css);
+	});
+});
