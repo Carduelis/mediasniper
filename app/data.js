66cb8718a1728762d1269bd3008ef9efa6976db8
Carduelis <pavepy@gmail.com>
2018-02-08 12:48:10 +0300

    adding data, refactor nav-menu into a loop

---
+++ app/data.js
@@ -0,0 +1,45 @@
+module.exports = {
+	slides: [
+		{
+			id: "01",
+			title: "Главная",
+			anchor: "slide01",
+			active: true
+		},
+		{
+			id: "02",
+			title: "О нас",
+			anchor: "slide02"
+		},
+		{
+			id: "03",
+			title: "Награды",
+			anchor: "slide03"
+		},
+		{
+			id: "04",
+			title: "Форматы",
+			anchor: "slide04"
+		},
+		{
+			id: "05",
+			title: "Инструменты",
+			anchor: "slide05"
+		},
+		{
+			id: "06",
+			title: "ATD",
+			anchor: "slide06"
+		},
+		{
+			id: "07",
+			title: "Клиенты",
+			anchor: "slide07"
+		},
+		{
+			id: "08",
+			title: "Контакты",
+			anchor: "slide08"
+		}
+	]
+};
