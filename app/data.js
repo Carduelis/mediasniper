6a74640ed1d588db428b807e376d45e65ee2561a
Carduelis <pavepy@gmail.com>
2018-02-08 19:09:27 +0300

    js updates

--- app/data.js
+++ app/data.js
@@ -1,43 +1,73 @@
 module.exports = {
 	slides: [
 		{
-			id: "01",
+			titleId: "01",
+			type: "main",
 			title: "Главная",
 			anchor: "slide01",
+			color: "white",
 			active: true
 		},
 		{
-			id: "02",
+			titleId: "02",
+			type: "main",
 			title: "О нас",
+			color: "black",
 			anchor: "slide02"
 		},
 		{
-			id: "03",
+			titleId: "03",
+			type: "main",
 			title: "Награды",
+			color: "white",
 			anchor: "slide03"
 		},
 		{
-			id: "04",
+			titleId: "04",
+			type: "main",
+			color: "black",
 			title: "Форматы",
-			anchor: "slide04"
+			anchor: "slide04-1"
 		},
 		{
-			id: "05",
+			titleId: null,
+			type: "sub",
+			color: "black",
+			title: "Форматы",
+			anchor: "slide04-2"
+		},
+		{
+			titleId: null,
+			type: "sub",
+			color: "black",
+			title: "Форматы",
+			anchor: "slide04-3"
+		},
+		{
+			titleId: "05",
+			type: "main",
+			color: "black",
 			title: "Инструменты",
 			anchor: "slide05"
 		},
 		{
-			id: "06",
+			titleId: "06",
+			color: "black",
+			type: "main",
 			title: "ATD",
 			anchor: "slide06"
 		},
 		{
-			id: "07",
+			titleId: "07",
+			type: "main",
+			color: "white",
 			title: "Клиенты",
 			anchor: "slide07"
 		},
 		{
-			id: "08",
+			titleId: "08",
+			type: "main",
+			color: "white",
 			title: "Контакты",
 			anchor: "slide08"
 		}
--- app/index.js
+++ app/index.js
@@ -2,10 +2,71 @@ import "./styles/style.less";
 import $ from "jquery";
 import "fullpage.js/dist/jquery.fullPage.js";
 const rootElement = document.getElementById("root");
+import { slides } from "./data";
+
+const selector = str => `[data-selector="${str}"]`;
+const getSlideByIndex = index => slides[index - 1];
+const getSlideEl = slide => $(`[data-slide="${slide.titleId}"]`);
 
 $(document).ready(function() {
-	$("#fullpage").fullpage();
-	$(".screen-card").each(function() {
+	$("#fullpage").fullpage({
+		anchors: slides.map(slide => slide.anchor),
+		menu: selector("menu"),
+		onLeave: function(index, nextIndex, direction) {
+			console.log(`${direction} from ${index} to ${nextIndex}`);
+			const slide = getSlideByIndex(index);
+			const nextSlide = getSlideByIndex(nextIndex);
+			$(".menu").attr("data-color", nextSlide.color);
+
+			reRunAnimationButton(getSlideEl(nextSlide).find(".button"));
+		},
+		afterLoad: function(anchorLink, index) {
+			const slide = getSlideByIndex(index);
+			console.log(anchorLink, index, slide);
+			const $slide = getSlideEl(slide);
+			$(".menu").attr("data-color", slide.color);
+			aliveScreenSplashes($slide);
+		},
+		afterRender: function() {
+			console.log(arguments);
+		},
+		afterResize: function() {},
+		afterResponsive: function(isResponsive) {},
+		afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {
+			console.log(arguments);
+			// aliveScreenSplashes($())
+		},
+		onSlideLeave: function(
+			anchorLink,
+			index,
+			slideIndex,
+			direction,
+			nextSlideIndex
+		) {
+			// wtf
+			console.log(anchorLink, index, slideIndex, direction, nextSlideIndex);
+		}
+	});
+});
+
+function reRunAnimationButton($btn) {
+	$btn.find("span").css("opacity", 0);
+
+	setTimeout(function() {
+		setTimeout(function() {
+			$btn.find("span").removeAttr("style");
+		}, 100);
+		reanimate($btn);
+	}, 300);
+}
+
+function reanimate($el) {
+	$el.removeClass("animated");
+	setTimeout(() => $el.addClass("animated"), 10);
+}
+
+function aliveScreenSplashes($el) {
+	$el.find(".screen-card").each(function() {
 		const $card = $(this);
 		const SCREEN_WIDTH = 1024;
 		const SCREEN_HEIGHT = 768;
@@ -19,4 +80,4 @@ $(document).ready(function() {
 		});
 		$card.find("span").css(css);
 	});
-});
+}
