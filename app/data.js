71e1c6b95081a92e60390134587c28e3356e6249
Pavel <pavepy@yandex.ru>
2018-02-17 02:52:36 +0300

    clients update

--- app/data.js
+++ app/data.js
@@ -74,77 +74,134 @@ module.exports = {
 	],
 	clients: [
 		{
-			name: 'Bayer',
-			image: './assets/client-1.png',
-		},
-		{
-			name: 'Крокус Сити Молл',
-			image: './assets/client-2.png'
-		},{
-			name: 'Lada'
+			name: "Эстетика",
+			link: "http://www.estetica.ru",
+			image: "./assets/clients/estetica.png"
 		}, {
-			name: 'Bosch'
-		},
+			name: "Bayer",
+			link: "https://www.bayer.com/",
+			image: "./assets/clients/bayer.png"
+		}, {
+			name: "Финам",
+			link: "http://forex.finam.ru",
+			image: "./assets/clients/finam.png"
+		}, {
+			name: "Лада",
+			link: "http://www.lada.ru/cars/vesta/sedan",
+			image: "./assets/clients/lada.png"
+		}, {
+			name: "Bosch",
+			link: "http://www.bosch-home.ru",
+			image: "./assets/clients/Bosch.png"
+		}, {
+			name: "Brabantia",
+			link: "https://www.brabantia-shop.ru",
+			image: "./assets/clients/brabantia.png"
+		}, {
+			name: "Button Blue",
+			link: "https://www.button-blue.com",
+			image: "./assets/clients/buttonblue.webp"
+		}, {
+			name: "Ситилинк",
+			link: "https://www.citilink.ru",
+			image: "./assets/clients/sitilink.png"
+		}, {
+			name: "Крокус сити холл",
+			link: "http://crocuscitymall.ru",
+			image: "./assets/clients/krokus.png "
+		}, {
+			name: "Datsun",
+			link: "https://www.datsun.ru",
+			image: "./assets/clients/datsun.png"
+		}, {
+			name: "Dunlop",
+			link: "http://wm02.dunlop-tire.ru",
+			image: "./assets/clients/dunlop.png"
+		}, {
+			name: "жк рассказоvо",
+			link: "http://www.sz-rasskazovo.ru",
+			image: "./assets/clients/rasskazovo.svg"
+		}, {
+			name: "Элевит Пронаталь",
+			link: "https://www.elevite.ru",
+			image: "./assets/clients/elevit.svg"
+		}, {
+			name: "Ford",
+			link: "http://www.ford.ru",
+			image: "./assets/clients/ford.png"
+		}, {
+			name: "GLO",
+			link: "https://www.myglo.ru",
+			image: "./assets/clients/glo.png"
+		}, {
+			name: "GoAnalitics",
+			link: "http://www.goanalytics.ru",
+			image: "./assets/clients/goa-logo.png"
+		}, {
+			name: "Hankook",
+			link: "http://www.hankooktire.com",
+			image: "./assets/clients/Hankook.png"
+		}, {
+			name: "Heinz",
+			link: "http://heinz-baby.ru",
+			image: "./assets/clients/heinz_logo.png"
+		}, {
+			name: "Henkel",
+			link: "https://got2b.promo.schwarzkopf.ru",
+			image: "./assets/clients/2000px-Henkel-Logo.svg.png"
+		}, {
+			name: "Hochland",
+			link: "http://hochland.ru",
+			image: "./assets/clients/hoch-land-9.png"
+		}, {
+			name: "HP",
+			link: "http://www8.hp.com/ru/ru/home.html",
+			image: "./assets/clients/2000px-HP_logo_2012.svg.png"
+		}, {
+			name: "Hyundai",
+			link: "http://www.hyundai.ru/NewSolaris",
+			image: "./assets/clients/Hyundai_logo.png"
+		}, {
+			name: "Infiniti",
+			link: "https://www.infiniti.ru/",
+			image: "./assets/clients/infiniti.png"
+		}, {
+			name: "Lexus",
+			link: "http://www.lexus.ru",
+			image: "./assets/clients/Lexus_logo.png"
+		}
+	],
+	tools: [
 		{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
-		},{
-
+			name: 'Динамические креативы',
+			description: 'Персонализированные баннеры для каждой группы целевой аудитории, меняющиеся в зависимости от их поведения и/или предпочтений.'
+		}, {
+			name: 'Кастомные сегменты',
+			description: 'Построение уникальных и специфических сегментов аудитории под конкретного рекламодателя / рекламную кампанию / продукт.'
+		}, {
+			name: 'Контекстный таргетинг',
+			description: 'Работает с контекстом окружения рекламного места на основе анализа семантики страницы. Демонстрация креативов происходит на площадках, где встречаются заранее подобранные ключевые слова.'
+		}, {
+			name: 'Brand Safety',
+			description: 'Защита бренда и его рекламных материалов от неблагоприятного информационного окружения.'
+		}, {
+			name: 'Ретаргетинг',
+			description: 'Персонализированное обращение через баннеры к пользователям, которые уже просмотрели рекламируемый продукт, посетив сайт рекламодателя.'
+		}, {
+			name: 'Look-alike',
+			description: 'Таргетинг, благодаря которому товар или услугу предлагают пользователям, по многим показателям похожим на существующих клиентов рекламодателя.'
+		}, {
+			name: 'Статистика Real-time',
+			description: 'Доступ к статистике рекламной кампании в режиме реального времени.'
+		}, {
+			name: 'Оптимизация по CPM, CPC, CPA, CPnBV, CPS',
+			description: 'Оптимизация рекламной кампании на достижение заданных рекламодателем показателей – стоимость сеанса, стоимость целевого действия и др.'
+		}, {
+			name: 'TV Sync',
+			description: 'Технология единовременной и ситуативной синхронизации интернет-рекламы с событиями на TV, выходом собственного или конкурирующего ролика.'
+		}, {
+			name: 'Brand lift',
+			description: 'Оценка уровня влияния интернет рекламы на рост узнаваемости или лояльности к бренду.'
 		}
 
 	]
