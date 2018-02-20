61ec764e23f1b5d5db9506ff2f833aa8ec1d72db
Carduelis <pavepy@gmail.com>
2018-02-20 15:32:18 +0300

    added cases

--- app/data.js
+++ app/data.js
@@ -77,95 +77,118 @@ module.exports = {
 			name: "Эстетика",
 			link: "http://www.estetica.ru",
 			image: "./assets/clients/estetica.png"
-		}, {
+		},
+		{
 			name: "Bayer",
 			link: "https://www.bayer.com/",
 			image: "./assets/clients/bayer.png"
-		}, {
+		},
+		{
 			name: "Финам",
 			link: "http://forex.finam.ru",
 			image: "./assets/clients/finam.png"
-		}, {
+		},
+		{
 			name: "Bosch",
 			link: "http://www.bosch-home.ru",
 			image: "./assets/clients/Bosch.png"
-		}, {
+		},
+		{
 			name: "Brabantia",
 			link: "https://www.brabantia-shop.ru",
 			image: "./assets/clients/brabantia.png"
-		}, {
+		},
+		{
 			name: "Button Blue",
 			link: "https://www.button-blue.com",
 			image: "./assets/clients/buttonblue.webp"
-		}, {
+		},
+		{
 			name: "Ситилинк",
 			link: "https://www.citilink.ru",
 			image: "./assets/clients/sitilink.png"
-		}, {
+		},
+		{
 			name: "Крокус сити холл",
 			link: "http://crocuscitymall.ru",
 			image: "./assets/clients/krokus.png "
-		}, {
+		},
+		{
 			name: "Datsun",
 			link: "https://www.datsun.ru",
 			image: "./assets/clients/datsun.png"
-		}, {
+		},
+		{
 			name: "Dunlop",
 			link: "http://wm02.dunlop-tire.ru",
 			image: "./assets/clients/dunlop.png"
-		}, {
+		},
+		{
 			name: "жк рассказоvо",
 			link: "http://www.sz-rasskazovo.ru",
 			image: "./assets/clients/rasskazovo.svg"
-		}, {
+		},
+		{
 			name: "Элевит Пронаталь",
 			link: "https://www.elevite.ru",
 			image: "./assets/clients/elevit.svg"
-		}, {
+		},
+		{
 			name: "Ford",
 			link: "http://www.ford.ru",
 			image: "./assets/clients/ford.png"
-		}, {
+		},
+		{
 			name: "GLO",
 			link: "https://www.myglo.ru",
 			image: "./assets/clients/glo.png"
-		}, {
+		},
+		{
 			name: "GoAnalitics",
 			link: "http://www.goanalytics.ru",
 			image: "./assets/clients/goa-logo.png"
-		}, {
+		},
+		{
 			name: "Hankook",
 			link: "http://www.hankooktire.com",
 			image: "./assets/clients/Hankook.png"
-		}, {
+		},
+		{
 			name: "Heinz",
 			link: "http://heinz-baby.ru",
 			image: "./assets/clients/heinz_logo.png"
-		}, {
+		},
+		{
 			name: "Henkel",
 			link: "https://got2b.promo.schwarzkopf.ru",
 			image: "./assets/clients/2000px-Henkel-Logo.svg.png"
-		}, {
+		},
+		{
 			name: "Hochland",
 			link: "http://hochland.ru",
 			image: "./assets/clients/hoch-land-9.png"
-		}, {
+		},
+		{
 			name: "HP",
 			link: "http://www8.hp.com/ru/ru/home.html",
 			image: "./assets/clients/2000px-HP_logo_2012.svg.png"
-		}, {
+		},
+		{
 			name: "Hyundai",
 			link: "http://www.hyundai.ru/NewSolaris",
 			image: "./assets/clients/Hyundai_logo.png"
-		}, {
+		},
+		{
 			name: "Infiniti",
 			link: "https://www.infiniti.ru/",
 			image: "./assets/clients/infiniti.png"
-		}, {
+		},
+		{
 			name: "Lexus",
 			link: "http://www.lexus.ru",
 			image: "./assets/clients/Lexus_logo.png"
-		}, {
+		},
+		{
 			name: "Лада",
 			link: "http://www.lada.ru/cars/vesta/sedan",
 			image: "./assets/clients/lada.png"
@@ -173,60 +196,84 @@ module.exports = {
 	],
 	tools: [
 		{
-			name: 'Динамические креативы',
-			description: 'Персонализированные баннеры для каждой группы целевой аудитории, меняющиеся в зависимости от их поведения и/или предпочтений.'
-		}, {
-			name: 'Кастомные сегменты',
-			description: 'Построение уникальных и специфических сегментов аудитории под конкретного рекламодателя / рекламную кампанию / продукт.'
-		}, {
-			name: 'Контекстный таргетинг',
-			description: 'Работает с контекстом окружения рекламного места на основе анализа семантики страницы. Демонстрация креативов происходит на площадках, где встречаются заранее подобранные ключевые слова.'
-		}, {
-			name: 'Brand Safety',
-			description: 'Защита бренда и его рекламных материалов от неблагоприятного информационного окружения.'
-		}, {
-			name: 'Ретаргетинг',
-			description: 'Персонализированное обращение через баннеры к пользователям, которые уже просмотрели рекламируемый продукт, посетив сайт рекламодателя.'
-		}, {
-			name: 'Look-alike',
-			description: 'Таргетинг, благодаря которому товар или услугу предлагают пользователям, по многим показателям похожим на существующих клиентов рекламодателя.'
-		}, {
-			name: 'Статистика Real-time',
-			description: 'Доступ к статистике рекламной кампании в режиме реального времени.'
-		}, {
-			name: 'Оптимизация по CPM, CPC, CPA, CPnBV, CPS',
-			description: 'Оптимизация рекламной кампании на достижение заданных рекламодателем показателей – стоимость сеанса, стоимость целевого действия и др.'
-		}, {
-			name: 'TV Sync',
-			description: 'Технология единовременной и ситуативной синхронизации интернет-рекламы с событиями на TV, выходом собственного или конкурирующего ролика.'
-		}, {
-			name: 'Brand lift',
-			description: 'Оценка уровня влияния интернет рекламы на рост узнаваемости или лояльности к бренду.'
+			name: "Динамические креативы",
+			description:
+				"Персонализированные баннеры для каждой группы целевой аудитории, меняющиеся в зависимости от их поведения и/или предпочтений."
+		},
+		{
+			name: "Кастомные сегменты",
+			description:
+				"Построение уникальных и специфических сегментов аудитории под конкретного рекламодателя / рекламную кампанию / продукт."
+		},
+		{
+			name: "Контекстный таргетинг",
+			description:
+				"Работает с контекстом окружения рекламного места на основе анализа семантики страницы. Демонстрация креативов происходит на площадках, где встречаются заранее подобранные ключевые слова."
+		},
+		{
+			name: "Brand Safety",
+			description:
+				"Защита бренда и его рекламных материалов от неблагоприятного информационного окружения."
+		},
+		{
+			name: "Ретаргетинг",
+			description:
+				"Персонализированное обращение через баннеры к пользователям, которые уже просмотрели рекламируемый продукт, посетив сайт рекламодателя."
+		},
+		{
+			name: "Look-alike",
+			description:
+				"Таргетинг, благодаря которому товар или услугу предлагают пользователям, по многим показателям похожим на существующих клиентов рекламодателя."
+		},
+		{
+			name: "Статистика Real-time",
+			description:
+				"Доступ к статистике рекламной кампании в режиме реального времени."
+		},
+		{
+			name: "Оптимизация по CPM, CPC, CPA, CPnBV, CPS",
+			description:
+				"Оптимизация рекламной кампании на достижение заданных рекламодателем показателей – стоимость сеанса, стоимость целевого действия и др."
+		},
+		{
+			name: "TV Sync",
+			description:
+				"Технология единовременной и ситуативной синхронизации интернет-рекламы с событиями на TV, выходом собственного или конкурирующего ролика."
+		},
+		{
+			name: "Brand lift",
+			description:
+				"Оценка уровня влияния интернет рекламы на рост узнаваемости или лояльности к бренду."
 		}
-
 	],
 	projects: [
 		{
-			id: 'nissan',
-			name: 'Nissan Murano',
-			image: './assets/cases/murano.jpg',
-			images: ['./assets/cases/infinity.jpg','./assets/cases/infinity.jpg','./assets/cases/infinity.jpg'],
-			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-		},{
-			id: 'infinity',
-			name: 'Infinity',
-			image: './assets/cases/infinity.jpg',
-			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-		},{
-			id: 'imho',
-			name: 'IMHO',
-			image: './assets/cases/horse.jpg',
-			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-		},{
-			id: 'ar',
-			name: 'Adrenalin Rush',
-			image: './assets/cases/adrenalin.jpg',
-			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+			id: "nissan",
+			name: "Nissan Murano",
+			image: "./assets/cases/murano.jpg",
+			images: ["./assets/cases/murano1.jpg", "./assets/cases/murano2.jpg"]
 		},
+		{
+			id: "infiniti",
+			name: "Infiniti",
+			image: "./assets/cases/infiniti.jpg",
+			images: [
+				"./assets/cases/infiniti1.jpg",
+				"./assets/cases/infiniti2.jpg",
+				"./assets/cases/infiniti3.jpg"
+			]
+		},
+		{
+			id: "imho",
+			name: "IMHO",
+			image: "./assets/cases/imho.jpg",
+			images: ["./assets/cases/imho1.jpg"]
+		},
+		{
+			id: "ar",
+			name: "Adrenalin Rush",
+			image: "./assets/cases/adrenaline.jpg",
+			images: ["./assets/cases/adrenaline1.jpg"]
+		}
 	]
 };
